3d26db6ed3d2731317624819bc40abfd2eb680b0
Ishan Jain <84621870+ishan29jain@users.noreply.github.com>
2025-02-24 17:26:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Tab Switching Logic
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -13,18 +13,85 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab");
 }
 
-// Typed.js for Dynamic Text
+
 var typed = new Typed('#element', {
     strings: ['Web Developer.', 'UI/UX Designer.', 'AI Enthusiast.'],
     typeSpeed: 100,
 });
 
-// Add or remove the "scrolled" class based on scroll position
+
+
+let lastScrollPosition = 0;
+
 window.addEventListener("scroll", function () {
     const nav = document.getElementById("navbar");
-    if (window.scrollY > 50) { // Add "scrolled" class after scrolling 50px
+    const currentScrollPosition = window.scrollY;
+
+    if (currentScrollPosition > lastScrollPosition && currentScrollPosition > 50) {
+
         nav.classList.add("scrolled");
     } else {
+
         nav.classList.remove("scrolled");
     }
+
+
+    lastScrollPosition = currentScrollPosition;
+    nav.addEventListener("mouseenter", () =>{
+        nav.classList.remove("scrolled");
+    })
+
+});
+
+const projectsContainer = document.getElementById("projectsContainer");
+
+
+const scrollSpeed = 4;
+
+
+function autoScroll() {
+    let currentScrollPosition = projectsContainer.scrollLeft;
+    const maxScrollWidth = projectsContainer.scrollWidth - projectsContainer.clientWidth;
+
+
+    if (currentScrollPosition < maxScrollWidth) {
+        projectsContainer.scrollTo({
+            left: currentScrollPosition + scrollSpeed,
+            behavior: "smooth",
+        });
+    } else {
+
+        // projectsContainer.scrollTo({
+        //     left: 0,
+        //     behavior: "smooth",
+        // });
+    }
+}
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                
+                scrollInterval = setInterval(autoScroll, 100);
+            } else {
+                
+                clearInterval(scrollInterval);
+            }
+        });
+    },
+    {
+        threshold: 0.5, 
+    }
+);
+
+
+observer.observe(projectsContainer);
+
+projectsContainer.addEventListener("mouseenter", () => {
+    clearInterval(scrollInterval);
+});
+
+
+projectsContainer.addEventListener("mouseleave", () => {
+    scrollInterval = setInterval(autoScroll, 100);
 });
